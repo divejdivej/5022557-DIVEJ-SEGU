28a03851df55ed00c2dcdbb9ad3292b7531d14e9
divejdivej <139539204+divejdivej@users.noreply.github.com>
2024-08-28 16:53:40 +0530

    Delete WEEK 5 ASSIGNMENT SUBMISSION/EXERCISE 8 WEEK 5/BookstoreAPI/src directory

--- WEEK 5 ASSIGNMENT SUBMISSION/EXERCISE 8 WEEK 5/BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BookstoreAPI.repository;
-
-import com.example.BookstoreAPI.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
